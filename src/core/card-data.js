d91f18f79c446623b65541b419991e4065a2b423
Kesler Tanner <keslert@gmail.com>
2019-11-23 18:28:43 -0500

    Fix

--- src/core/card-data.js
+++ src/core/card-data.js
@@ -2,206 +2,206 @@ export default [
   {
     text: 'You never have to work for money again. What do you do with your time?',
     tags: [''],
-    number: 1,
   },{
     text: 'Dementors are closing in and you must cast a powerful patronus. What memory do you turn to?',
     tags: ['Harry Potter', 'Memory'],
-    number: 2,
   },{
     text: "You're so angry you could cast an Unforgiveable Curse. What just happened?",
     tags: ['Harry Potter'],
-    number: 3,
   },{
     text: "You've just received a $5 million investment for your charity. What cause are you fighting for?",
     tags: ['Charity'],
-    number: 4,
   },{
     text: "What aspects of humans have made us a successful species?",
     tags: ['Human Nature'],
-    number: 5,
   },{
     text: "The book you're reading is in danger of bursting into flames you're turning the pages so fast. What's the book?",
     tags: ['Favorites'],
-    number: 6,
   },{
     text: "Sitting by the fireplace, you can't help but recall your favorite Christmas of years past. What made it so special?",
     tags: ['Memory', 'Christmas'],
-    number: 7,
   },{
     text: "You've just been hired to build the perfect city. What's your plan?",
     tags: ['Imagination'],
-    number: 8,
   },{
     text: "It was just announced in the news that the world supply of ________ might be poisoned. You think you'll risk still eating it.",
     tags: ['Favorites'],
-    number: 9,
   },{
     text: 'What makes you feel accomplished?',
     tags: ['Motivation'],
-    number: 10,
   },{
     text: 'What’s the one thing that people always misunderstand about you?',
     tags: [''],
-    number: 11,
   },{
     text: 'What is your idea of a perfect vacation?',
     tags: [''],
-    number: 12,
   },{
     text: 'When have you felt your biggest adrenaline rush?',
     tags: [''],
-    number: 13,
   },{
     text: 'Where is your favorite place in the entire world to go?',
     tags: [''],
-    number: 14,
   },{
     text: 'What is the weirdest thing about you?',
     tags: [''],
-    number: 15,
   },{
     text: "You've saved your money for years to go on this trip. Tell us about it.",
     tags: [''],
-    number: 16,
   },{
     text: 'What song makes you unconditionally happy?',
     tags: [''],
-    number: 17,
   },{
     text: 'If you could have anybody else’s life, who’s would you take?',
     tags: [''],
-    number: 18,
   },{
     text: 'Is there an activity that calms you? A place or a thing that makes you feel at ease?',
     tags: [''],
-    number: 19,
   },{
     text: 'What scene in a movie has evoked the most feelings out of you?',
     tags: [''],
-    number: 20,
   },{
     text: "What's a change you made in your life that still leave you feeling proud?",
     tags: [''],
-    number: 21,
   },{
     text: 'Think about the people you love the most in your life, what do you do for them?',
     tags: [''],
-    number: 22,
   },{
     text: 'Out of the negative emotions of greed, anger, jealousy and hate, which one would you say affects you the most?',
     tags: [''],
-    number: 23,
   },{
     text: 'Out of the positive emotions of compassion, positivity, enthusiasm and initiative, which one is your biggest strength?',
     tags: [''],
-    number: 24,
   },{
     text: 'What do you miss about being a kid?',
     tags: [''],
-    number: 25,
   },{
     text: 'What’s something you want to do in the next year that you’ve never done before?',
     tags: [''],
-    number: 26,
   },{
     text: 'You found yourself feeling unexpectedly grateful for something this week. What was it?',
     tags: [''],
-    number: 27,
   },{
     text: 'How do you best connect with others?',
     tags: [''],
-    number: 28,
   },{
     text: 'What’s some of the best advice you’ve ever gotten?',
     tags: [''],
-    number: 29,
   },{
     text: 'If you could take a year-long paid sabbatical, what would you do?',
     tags: [''],
-    number: 30,
   },{
     text: 'If you could enter a time machine, what time period would you go to?',
     tags: [''],
-    number: 31,
   },{
     text: 'Where was your favorite place to go as a child and why did you love it there?',
     tags: [''],
-    number: 32,
   },{
     text: 'If you could have any job you wanted, what would it be?',
     tags: [''],
-    number: 33,
   },{
     text: 'If there were 26 hours in a day, what would you do more of?',
     tags: [''],
-    number: 34,
   },{
     text: 'What moments or events during your childhood shaped who you are now?',
     tags: [''],
-    number: 35,
   },{
     text: "How do you balance chasing money, prestige and/or high powered positions with being content with what you've obtained? Where do you draw the line?",
     tags: [''],
-    number: 36,
   },{
     text: 'If you could write a fiction novel, describe your protagonist?',
     tags: [''],
-    number: 37,
   },{
     text: "What's your ideal retirement?",
     tags: [''],
-    number: 38,
   },{
     text: "You've retired with $3 million—How do you spend your money?",
     tags: [''],
-    number: 39,
   },{
     text: "You've just started a t-shirt line? What's your thing?",
     tags: [''],
-    number: 40,
   },{
     text: "What's something that most people consider as common knowledge for adult, but you're still in the dark?",
     tags: [''],
-    number: 41,
   },{
     text: "What's one job perk that might sway you into joining a company?",
     tags: [''],
-    number: 42,
   },{
     text: "What's something that costs $50 or less that's a huge value add for your life?",
     tags: [''],
-    number: 43,
   },{
     text: "What's your favorite purchase?",
     tags: [''],
-    number: 44,
   },{
     text: "What pair of shoes have brought you the most satisfaction?",
     tags: [''],
-    number: 45,
   },{
     text: "You feel a touch on your shoulder. \"Are you sleeping in class again?\" You wake up in your 7th grade math class and realize your life up to this point was just a dream. How do you react and what do plan to do?",
     tags: [''],
-    number: 46,
   },{
     text: "Without saying what the category is, what are your top five?",
     tags: [''],
-    number: 47,
   },{
     text: "What's an \"oh no\" moment where you realised you've been doing something the wrong way for years?",
     tags: [''],
-    number: 48,
   },{
     text: "Besides eating cereal with water, what is the most outrageous \"eating sin\" you have ever witnessed?",
     tags: [''],
-    number: 49,
   },{
     text: "What is something you find super important that is on the verge of happening?",
     tags: [''],
-    number: 50,
   },{
     text: "What is the most kind/wholesome thing you've overhead about you or someone you know by accident?",
     tags: [''],
-    number: 51,
-  }
-]
+  },{
+    text: "If you had to blow $500 in 3 days, how would you spend it?",
+    tags: [''],
+  },{
+    text: "If you were going to be a one-hit wonder as a movie star, what movie would you like to star in the remake of?",
+    tags: [''],
+  },{
+    text: "What keeps you up at night?",
+    tags: [''],
+  },{
+    text: "What blue-collar job would you be good at? What white-collar job would you be good at?",
+    tags: [''],
+  },{
+    text: "What fake expletive do you use?",
+    tags: [''],
+  },{
+    text: "What easy thing do you most often screw up?",
+    tags: [''],
+  },{
+    text: "What's one common thing that people do that you just can't stand?",
+    tags: [''],
+  },{
+    text: "What rule of etiquette do you think should be tossed?",
+    tags: [''],
+  },{
+    text: "What do you think the role of a woman in a relationship should be?",
+    tags: [''],
+  },{
+    text: "What do you think is worth spending extra on to get the best?",
+    tags: [''],
+  },{
+    text: "What do you think is a waste of money?",
+    tags: [''],
+  },{
+    text: "What do you know that you aren’t supposed to know?",
+    tags: [''],
+  },{
+    text: "What 30 seconds would you erase from your life if you could?",
+    tags: [''],
+  },{
+    text: "What’s a good relationship tip that sounds like a bad tip?",
+    tags: [''],
+  },{
+    text: "What do you need to vent about?",
+    tags: [''],
+  },{
+    text: "What causes you to look down on someone?",
+    tags: [''],
+  },{
+    text: "What are some things you are below average at and some things you are above average at?",
+    tags: [''],
+  },
+].map((q, i) => ({...q, number: i}))
